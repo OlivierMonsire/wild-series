892ae4a755eef2b76d6bc89212ee59a6013991da
Olivier <olivier.monsire@gmail.com>
2020-05-10 11:18:05 +0200

    add name field in category table

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -17,8 +17,25 @@ class Category
      */
     private $id;
 
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $name;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
     }
 }
