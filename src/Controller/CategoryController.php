a4d7f3d3b989bf592a193dceb114d7ab18bcdf78
Olivier <olivier.monsire@gmail.com>
2020-05-20 17:06:10 +0200

    Symfony Quest 12

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -4,62 +4,91 @@ namespace App\Controller;
 
 use App\Entity\Category;
 use App\Form\CategoryType;
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/category")
+ */
 class CategoryController extends AbstractController
 {
-
     /**
-     * Show categories
-     *
-     * @Route("/category/index", name="category_index")
+     * @Route("/", name="category_index", methods={"GET"})
      */
-    public function index()
+    public function index(CategoryRepository $categoryRepository): Response
     {
-        $repository = $this->getDoctrine()
-            ->getManager()
-            ->getRepository(Category::class);
-        $categories = $repository->findAll();
-
-        return $this->render(
-            'category/index.html.twig', [
-            'categories' => $categories
+        return $this->render('category/index.html.twig', [
+            'categories' => $categoryRepository->findAll(),
         ]);
     }
 
     /**
-     * Add a new category
-     *
-     * @Route("/category/add", name="category_add")
-     * @param Request $request
-     * @return Response
+     * @Route("/new", name="category_new", methods={"GET","POST"})
      */
-    public function add(Request $request): Response
+    public function new(Request $request): Response
     {
-        $repository = $this->getDoctrine()
-            ->getManager()
-            ->getRepository(Category::class);
-        $categories = $repository->findAll();
-
         $category = new Category();
         $form = $this->createForm(CategoryType::class, $category);
-        $form->HandleRequest($request);
+        $form->handleRequest($request);
 
-        if ($form->isSubmitted()) {
-            $data = $form->getData();
-            $category = $this->getDoctrine()->getManager();
-            $category->persist($data);
-            $category->flush();
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($category);
+            $entityManager->flush();
 
+            return $this->redirectToRoute('category_index');
         }
 
-        return $this->render(
-            'category/add.html.twig', [
-            'categories' => $categories,
+        return $this->render('category/new.html.twig', [
+            'category' => $category,
             'form' => $form->createView(),
         ]);
+    }
+
+    /**
+     * @Route("/{id}", name="category_show", methods={"GET"})
+     */
+    public function show(Category $category): Response
+    {
+        return $this->render('category/show.html.twig', [
+            'category' => $category,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="category_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Category $category): Response
+    {
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('category_index');
+        }
+
+        return $this->render('category/edit.html.twig', [
+            'category' => $category,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="category_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Category $category): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$category->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($category);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('category_index');
     }
 }
--- src/Form/EpisodeType.php
+++ src/Form/EpisodeType.php
@@ -2,24 +2,27 @@
 
 namespace App\Form;
 
-use App\Entity\Category;
+use App\Entity\Episode;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class CategoryType extends AbstractType
+class EpisodeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
+            ->add('title')
+            ->add('number')
+            ->add('synopsis')
+            ->add('season', null, ['choice_label' => 'number'])
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Category::class,
+            'data_class' => Episode::class,
         ]);
     }
 }
